8c8afb34ce5ad888b2956d37f38871ab23b10a96
HaidenRothwell <46227616+hrothwell@users.noreply.github.com>
2024-10-11 16:09:24 -0700

    build

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,14 +10,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation( "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
 kotlin {
     jvmToolchain(17)
 }
