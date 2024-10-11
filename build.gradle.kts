9dd8886db564755777a08db95224c73146c5f7c0
HaidenRothwell <haiden.rothwell@target.com>
2024-10-11 16:04:41 -0700

    TwoDTree and friends

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation( "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("MainKt")
+}
